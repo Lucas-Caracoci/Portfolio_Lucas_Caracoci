88dd1db25b43a9d69e2a9c81c542d4ee46a1452b
Lucas Caracoci <127033819+Lucas-Caracoci@users.noreply.github.com>
2024-07-31 12:30:44 -0300

    Update reveal.js

--- reveal.js
+++ reveal.js
@@ -1,10 +1,10 @@
-window.revelar = ScrollReveal({ reset: true });
+window.revelar = ScrollReveal({ reset: false });
   
   var slideUp = {
     distance: '200px',
     origin: 'bottom',
     opacity: 1,
-    interval: 2,
+    interval: 200,
     
   };
 
