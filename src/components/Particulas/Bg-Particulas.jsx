e19e4cfcc3f4374216bdbf72662a1b46bde1d9f3
Lucas Caracoci <lucascaracoci2@gmail.com>
2024-11-07 14:30:56 -0300

    correção reveal banner e funcionalidade skills

--- src/components/Particulas/Bg-Particulas.jsx
+++ src/components/Particulas/Bg-Particulas.jsx
@@ -30,24 +30,7 @@ const BgParticulas = (props) => {
           value: "#fff"
         }
       },
-      backgroundMask: {
-        composite: "destination-out",
-        cover: {
-          color: {
-            value: "#242424"
-          },
-          opacity: 1
-        },
-        enable: false
-      },
-      clear: true,
-      defaultThemes: {},
       delay: 0,
-      fullScreen: {
-        enable: true,
-        
-      },
-      detectRetina: true,
       duration: 0,
       fpsLimit: 120,
       interactivity: {
@@ -57,18 +40,12 @@ const BgParticulas = (props) => {
             enable: true,
             mode: "repulse"
           },
-          onDiv: {
-            selectors: {},
-            enable: false,
-            mode: {},
-            type: "circle"
-          },
           onHover: {
             enable: true,
             mode: "bubble",
             parallax: {
-              enable: false,
-              force: 1,
+              enable: true,
+              force: 4,
               smooth: 1000
             }
           },
@@ -78,57 +55,15 @@ const BgParticulas = (props) => {
           }
         },
         modes: {
-          trail: {
-            delay: 1,
-            pauseOnStop: false,
-            quantity: 1
-          },
-          attract: {
-            distance: 200,
-            duration: 0.4,
-            easing: "ease-out-quad",
-            factor: 1,
-            maxSpeed: 50,
-            speed: 1
-          },
-          bounce: {
-            distance: 200
-          },
           bubble: {
-            distance: 250,
+            distance: 200,
             duration: 2,
             mix: false,
-            opacity: 0,
-            size: 0,
-            divs: {
-              distance: 200,
-              duration: 0.4,
-              mix: false,
-              selectors: {}
+            opacity: 1,
+            size: 1,
+            color: {
+              value: "#e45b00"
             }
-          },
-          connect: {
-            distance: 80,
-            links: {
-              opacity: 0.5
-            },
-            radius: 60
-          },
-          grab: {
-            distance: 400,
-            links: {
-              blink: false,
-              consent: false,
-              opacity: 1
-            }
-          },
-          push: {
-            default: true,
-            
-            quantity: 4
-          },
-          remove: {
-            quantity: 2
           },
           repulse: {
             distance: 400,
@@ -137,102 +72,18 @@ const BgParticulas = (props) => {
             speed: 1,
             maxSpeed: 50,
             easing: "ease-out-quad",
-            divs: {
-              distance: 200,
-              duration: 0.4,
-              factor: 100,
-              speed: 1,
-              maxSpeed: 50,
-              easing: "ease-out-quad",
-              selectors: {}
-            }
           },
           slow: {
             factor: 3,
             radius: 200
           },
-          light: {
-            area: {
-              gradient: {
-                start: {
-                  value: "#ffffff"
-                },
-                stop: {
-                  value: "#000000"
-                }
-              },
-              radius: 1000
-            },
-            shadow: {
-              color: {
-                value: "#000000"
-              },
-              length: 2000
-            }
-          }
+          
         }
       },
       
       particles: {
-        bounce: {
-          horizontal: {
-            value: 1
-          },
-          vertical: {
-            value: 1
-          }
-        },
-        collisions: {
-          absorb: {
-            speed: 2
-          },
-          bounce: {
-            horizontal: {
-              value: 1
-            },
-            vertical: {
-              value: 1
-            }
-          },
-          enable: false,
-          maxSpeed: 50,
-          mode: "bounce",
-          overlap: {
-            enable: true,
-            retries: 0
-          }
-        },
         color: {
           value: [ "#242424"],
-          animation: {
-            h: {
-              count: 0,
-              enable: false,
-              speed: 1,
-              decay: 0,
-              delay: 0,
-              sync: true,
-              offset: 0
-            },
-            s: {
-              count: 0,
-              enable: false,
-              speed: 1,
-              decay: 0,
-              delay: 0,
-              sync: true,
-              offset: 0
-            },
-            l: {
-              count: 0,
-              enable: false,
-              speed: 1,
-              decay: 0,
-              delay: 0,
-              sync: true,
-              offset: 0
-            }
-          }
         },
         effect: {
           close: true,
@@ -246,39 +97,12 @@ const BgParticulas = (props) => {
             offset: 0,
             value: 90
           },
-          attract: {
-            distance: 200,
-            enable: false,
-            rotate: {
-              x: 3000,
-              y: 3000
-            }
-          },
-          center: {
-            x: 50,
-            y: 50,
-            mode: "percent",
-            radius: 0
-          },
+      
           decay: 0,
           distance: {},
           direction: "none",
           drift: 0,
           enable: true,
-          gravity: {
-            acceleration: 9.81,
-            enable: false,
-            inverse: false,
-            maxSpeed: 50
-          },
-          path: {
-            clamp: true,
-            delay: {
-              value: 0
-            },
-            enable: false,
-            options: {}
-          },
           outModes: {
             default: "out"
           },
@@ -288,18 +112,6 @@ const BgParticulas = (props) => {
             min: 0.1,
             max: 1
           },
-          spin: {
-            acceleration: 0,
-            enable: false
-          },
-          straight: false,
-          trail: {
-            enable: false,
-            length: 10,
-            fill: {}
-          },
-          vibrate: false,
-          warp: false
         },
         number: {
           density: {
@@ -311,7 +123,7 @@ const BgParticulas = (props) => {
             mode: "delete",
             value: 0
           },
-          value: 160
+          value: 200
         },
         opacity: {
           value: {
@@ -331,17 +143,6 @@ const BgParticulas = (props) => {
           }
         },
         reduceDuplicates: false,
-        shadow: {
-          blur: 0,
-          color: {
-            value: "#000"
-          },
-          enable: false,
-          offset: {
-            x: 0,
-            y: 0
-          }
-        },
         shape: {
           close: true,
           fill: true,
@@ -365,70 +166,7 @@ const BgParticulas = (props) => {
             destroy: "none"
           }
         },
-        stroke: {
-          width: 0
-        },
-        destroy: {
-          bounds: {},
-          mode: "none",
-          split: {
-            count: 1,
-            factor: {
-              value: 3
-            },
-            rate: {
-              value: {
-                min: 4,
-                max: 9
-              }
-            },
-            sizeOffset: true
-          }
-        },
-        roll: {
-          darken: {
-            enable: false,
-            value: 0
-          },
-          enable: false,
-          enlighten: {
-            enable: false,
-            value: 0
-          },
-          mode: "vertical",
-          speed: 25
-        },
-        tilt: {
-          value: 0,
-          animation: {
-            enable: false,
-            speed: 0,
-            decay: 0,
-            sync: false
-          },
-          direction: "clockwise",
-          enable: false
-        },
-        twinkle: {
-          lines: {
-            enable: false,
-            frequency: 0.05,
-            opacity: 1
-          },
-          particles: {
-            enable: false,
-            frequency: 0.05,
-            opacity: 1
-          }
-        },
-        wobble: {
-          distance: 5,
-          enable: false,
-          speed: {
-            angle: 50,
-            move: 10
-          }
-        },
+      
         life: {
           count: 0,
           delay: {
@@ -439,57 +177,6 @@ const BgParticulas = (props) => {
             value: 0,
             sync: false
           }
-        },
-        rotate: {
-          value: 0,
-          animation: {
-            enable: false,
-            speed: 0,
-            decay: 0,
-            sync: false
-          },
-          direction: "clockwise",
-          path: false
-        },
-        orbit: {
-          animation: {
-            count: 0,
-            enable: false,
-            speed: 1,
-            decay: 0,
-            delay: 0,
-            sync: false
-          },
-          enable: false,
-          opacity: 1,
-          rotation: {
-            value: 45
-          },
-          width: 1
-        },
-        links: {
-          blink: false,
-          color: {
-            value: "#fff"
-          },
-          consent: false,
-          distance: 100,
-          enable: false,
-          frequency: 1,
-          opacity: 1,
-          shadow: {
-            blur: 5,
-            color: {
-              value: "#000"
-            },
-            enable: false
-          },
-          triangles: {
-            enable: false,
-            frequency: 1
-          },
-          width: 1,
-          warp: false
         },
         repulse: {
           value: 0,
@@ -503,15 +190,9 @@ const BgParticulas = (props) => {
       pauseOnBlur: true,
       pauseOnOutsideViewport: true,
       smooth: false,
-      zLayers: 100,
+      zLayers: 1,
       name: "bg",
-      motion: {
-        disable: false,
-        reduce: {
-          factor: 4,
-          value: true
-        }
-      }
+      
     }),
     [],
   );
